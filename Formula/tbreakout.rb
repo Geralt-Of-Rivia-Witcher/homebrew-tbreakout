8bb21cc398ce3db8fac0a7010f5aa4cb07be3f58
Siddhant Kumar Singh <singhsiddhantkumar@gmail.com>
2026-03-04 21:23:10 +0530

    tbreakout v0.1.8 (#16)
    Co-authored-by: github-actions <github-actions@github.com>

--- Formula/tbreakout.rb
+++ Formula/tbreakout.rb
@@ -1,16 +1,16 @@
 class Tbreakout < Formula
   desc "Retro terminal Breakout game written in Go using tcell"
   homepage "https://github.com/Geralt-Of-Rivia-Witcher/tbreakout"
-  version "0.1.6"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Geralt-Of-Rivia-Witcher/tbreakout/releases/download/v0.1.6/tbreakout-darwin-arm64"
-      sha256 "981110dbdd86af6739afa5a62dcf06b6bccfe2f88c4aef32aa2b1783e467357c"
+      url "https://github.com/Geralt-Of-Rivia-Witcher/tbreakout/releases/download/v0.1.8/tbreakout-darwin-arm64"
+      sha256 "e50fdd34ead0b3c893752e40984671110af1ea810d73a7fdf4f4d0b16e9f2dd4"
     else
-      url "https://github.com/Geralt-Of-Rivia-Witcher/tbreakout/releases/download/v0.1.6/tbreakout-darwin-amd64"
-      sha256 "0fff1be256e1d54bc776440beb7700f47f9bbf2d2f26ed1a4fd9d8044c886fc4"
+      url "https://github.com/Geralt-Of-Rivia-Witcher/tbreakout/releases/download/v0.1.8/tbreakout-darwin-amd64"
+      sha256 "297b422f31781f8d7f94f1c538d077abdd507306a62cd3a1e3beab96e6cbe50c"
     end
   end
 
